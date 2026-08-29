5dfc6e222ad98ac46232ad9c68876bc5acf66bfe
Prachi Aggarwal <prachiagg7782@gmail.com>
2026-08-29 11:27:47 +0530

    Add root package.json and update backend routes for Netlify

--- server/server.js
+++ server/server.js
@@ -27,6 +27,15 @@ app.get('/api/health', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+const path = require('path');
+
+// Serve static files from the React dist folder
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
